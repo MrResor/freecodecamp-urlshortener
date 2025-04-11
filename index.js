8b2394b5839e86ad2311d7e7037d4a8f301c3ee0
Mr.Resor <jakusienko@gmail.com>
2025-04-11 16:06:48 +0000

    add the database connection object

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+
+import pg from 'pg'
+const { Pool } = pg
+
 const app = express();
 
 // Basic Configuration
-const port = process.env.PORT || 3000;
+const port = 3000;
+
+const pool  = new Pool({
+  user: `${process.env.USER_LOGIN}`,
+  password: `${process.env.USER_PASSWORD}`,
+  host: 'db',
+  port: 5432,
+  database: 'url_shortener',
+})
+
+await pool.connect()
 
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
