56d62e8a02ea4e2be131cf772b42e27fa97d8be8
Mr.Resor <jakusienko@gmail.com>
2025-04-13 20:35:30 +0000

    add endpoint stplit from initial index file

---
+++ src/api/docs/index.js
@@ -0,0 +1,14 @@
+import fs from "fs";
+
+import express from "express";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yaml";
+
+const file = fs.readFileSync('documentation.yaml', 'utf8');
+const swaggerDocument = YAML.parse(file);
+
+const docs = express.Router();
+
+docs.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+export { docs };
