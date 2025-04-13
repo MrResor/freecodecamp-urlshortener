a641666e7ecb60899edb667f5272c18ef828d52a
Mr.Resor <jakusienko@gmail.com>
2025-04-13 20:38:02 +0000

    move express declaration from initial index

---
+++ src/express.js
@@ -0,0 +1,62 @@
+import cors from 'cors';
+import express from 'express';
+import path from 'path';
+
+import { docs } from "./api/docs/index.js";
+import { hello } from "./api/hello/index.js";
+import { logger } from "./logger.js";
+import { mainView } from "./index.js";
+
+const app = express();
+const router = express.Router();
+const __dirname = import.meta.dirname;
+
+//Middleware declaration
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+router.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+router.use(express.static(path.join(__dirname, '..', 'public')));
+
+router.use((req, _, next) => {
+
+    let hasRouteToHandle = null;
+    router.stack.forEach((stackItem) => {
+        // check if current rout path matches route request path
+        if (stackItem.handle?.stack !== undefined) {
+            stackItem?.handle.stack.forEach((innerItem) => {
+                if (innerItem.regexp.test(req.path)) {
+                    hasRouteToHandle = true;
+                }
+            });
+        }
+    });
+
+    let ip = Object.hasOwn(req.headers, 'x-forwarded-for') ? req.headers['x-forwarded-for'] : req.ip;
+    let msg = `${req.method} ${req.path} - ${ip}`
+
+    if (hasRouteToHandle) {
+        logger.info(msg);
+    } else {
+        // No matching route for this request
+        logger.error(msg);
+    }
+
+    next();
+});
+
+//Routes declaration
+
+// /
+router.use(mainView);
+// /api/hello
+router.use(hello);
+// /api/docs
+router.use(docs);
+
+app.use(router);
+
+
+export { app };
