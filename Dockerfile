672c0a5099c5e77396d2495f46037f64649d172b
Mr.Resor <jakusienko@gmail.com>
2025-04-13 20:32:35 +0000

    clear up the file and add logger

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM node:latest
-COPY package.json package.json
-RUN npm install
-RUN npm audit fix
-EXPOSE 3000
-COPY . .
-CMD ["npm", "run", "start"]
--- index.js
+++ index.js
@@ -1,38 +1,7 @@
-import 'dotenv/config';
-import cors from 'cors';
-import express from 'express';
+import { app } from "./src/express.js";
+import { logger } from "./src/logger.js";
 
-import pg from 'pg'
-const { Pool } = pg
-
-const app = express();
-
-// Basic Configuration
-const port = 3000;
-
-const pool  = new Pool({
-  user: `${process.env.USER_LOGIN}`,
-  password: `${process.env.USER_PASSWORD}`,
-  host: 'db',
-  port: 5432,
-  database: 'url_shortener',
-})
-
-await pool.connect()
-
-app.use(cors());
-
-app.use('/public', express.static(`${process.cwd()}/public`));
-
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
-
-// Your first API endpoint
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
-});
-
-app.listen(port, function () {
-  console.log(`Listening on port ${port}`);
+// Listen on port set in environment variable or default to 3000
+var listener = app.listen(3000, function () {
+  logger.info('Your app is listening on port ' + listener.address().port);
 });
