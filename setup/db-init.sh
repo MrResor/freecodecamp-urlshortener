cb48ba7a0b1c5d291187c96871883b00b5347870
Mr.Resor <jakusienko@gmail.com>
2025-04-16 16:48:21 +0000

    add commant that grants usage on sequences in postgres

--- setup/db-init.sh
+++ setup/db-init.sh
@@ -2,3 +2,4 @@
 
 psql -U postgres -c "CREATE USER $PGUSER WITH PASSWORD '$PGPASSWORD'" -c "CREATE DATABASE url_shortener WITH OWNER = $PGUSER"
 psql -U postgres -d url_shortener -c "CREATE TABLE urls (ID SERIAL PRIMARY KEY, URL TEXT NOT NULL)" -c "GRANT SELECT, INSERT, UPDATE, DELETE ON urls TO $PGUSER"
+psql -U postgres -d url_shortener -c "GRANT USAGE, SELECT ON SEQUENCE urls_id_seq TO $PGUSER"
