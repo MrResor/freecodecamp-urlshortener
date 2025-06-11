e92a3ac1f2e1cc5130af1a7a1aa0715e0c2352a1
Mr.Resor <jakusienko@gmail.com>
2025-06-11 16:34:51 +0000

    add commands and split them so they are easier to read

--- setup/db-init.sh
+++ setup/db-init.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-psql -U postgres -c "CREATE USER $PGUSER WITH PASSWORD '$PGPASSWORD'" -c "CREATE DATABASE url_shortener WITH OWNER = $PGUSER"
-psql -U postgres -d url_shortener -c "CREATE TABLE urls (ID SERIAL PRIMARY KEY, URL TEXT NOT NULL)" -c "GRANT SELECT, INSERT, UPDATE, DELETE ON urls TO $PGUSER"
+psql -U postgres -c "CREATE USER $PGUSER WITH PASSWORD '$PGPASSWORD'"
+psql -U postgres -c "CREATE DATABASE url_shortener WITH OWNER = $PGUSER"
+psql -U postgres -d url_shortener -c "CREATE TABLE urls (ID SERIAL PRIMARY KEY, URL TEXT NOT NULL)"
+psql -U postgres -d url_shortener -c "GRANT SELECT, INSERT, UPDATE, DELETE ON urls TO $PGUSER"
 psql -U postgres -d url_shortener -c "GRANT USAGE, SELECT ON SEQUENCE urls_id_seq TO $PGUSER"
+psql -U postgres -d url_shortener -c "GRANT CONNECT ON DATABASE url_shortener TO $PGUSER"
+psql -U postgres -d url_shortener -c "GRANT USAGE ON SCHEMA public TO $PGUSER"
+psql -U postgres -d url_shortener -c "\du"
