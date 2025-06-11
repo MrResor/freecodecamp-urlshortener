a915f9e13ed20a945e954cd888e0c523d7ce9fca
Mr.Resor <jakusienko@gmail.com>
2025-06-11 18:52:57 +0000

    move :shorturl tests to respective file

---
+++ __tests__/:shorturl.test.mjs
@@ -0,0 +1,61 @@
+import 'dotenv/config'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it, vi } from 'vitest'
+
+import { setupContainer } from './container.mjs'
+
+let app, container, port
+
+const url = 'https://www.google.com'
+let id
+
+beforeAll(async () => {
+  [container, port] = await setupContainer(5432 + Number(process.env.VITEST_WORKER_ID))
+
+  vi.stubEnv('DB_PORT', port.toString())
+
+  // Import the app AFTER the container is up and env vars are set
+  const mod = await import('../src/express.js')
+  app = mod.app
+
+  const res = await request(app).post('/api/shorturl').send({ url }) // Add the URL to the database for testing
+
+  expect(res.statusCode).toBe(200)
+  expect(res.body).toEqual({
+    original_url: url,
+    short_url: expect.any(Number)
+  })
+
+  id = res.body.short_url // Store the short URL ID for later tests
+
+}, 60_000)
+
+afterAll(async () => {
+  if (container) {
+    await container.stop()
+  }
+  vi.unstubAllEnvs()
+})
+
+describe('/api/shorturl/:id', () => {
+  it('get url by id', async () => {
+    const res = await request(app).get(`/api/shorturl/${url}`)
+
+    expect(res.statusCode).toBe(301)
+    expect(res.headers.location).toBe('https://www.google.com')
+  })
+
+  it('try getting non-existing url', async () => {
+    const res = await request(app).get('/api/shorturl/9999')
+
+    expect(res.statusCode).toBe(404)
+    expect(res.body).toEqual({ error: 'No url found for the given ID' })
+  })
+
+  it('try getting url with invalid id', async () => {
+    const res = await request(app).get('/api/shorturl/invalid-id')
+
+    expect(res.statusCode).toBe(400)
+    expect(res.body).toEqual({ error: 'Invalid ID' })
+  })
+})
--- __tests__/shorturl.test.mjs
+++ __tests__/shorturl.test.mjs
@@ -23,49 +23,24 @@ afterAll(async () => {
   vi.unstubAllEnvs()
 })
 
-let url
-
 describe('/api/shorturl', () => {
   it('add url', async () => {
-    const res = await await request(app).post('/api/shorturl').send({ url: "https://www.google.com" })
+    const res = await await request(app).post('/api/shorturl').send({ url: 'https://www.google.com' })
 
     url = res.body.short_url // Store the short URL for later tests
 
     expect(res.statusCode).toBe(200)
-    expect(res.body).toEqual({ 
+    expect(res.body).toEqual({
       original_url: 'https://www.google.com',
       short_url: expect.any(Number)
     })
   })
 
-  it('try adding invalid url', async () => {  
-    const res = await request(app).post('/api/shorturl').send({ url: "invalid-url" })
+  it('try adding invalid url', async () => {
+    const res = await request(app).post('/api/shorturl').send({ url: 'invalid-url' })
 
     expect(res.statusCode).toBe(400)
     expect(res.body).toEqual({ error: 'Invalid URL' })
   })
-
 })
 
-describe('/api/shorturl/:id', () => {
-  it('get url by id', async () => {
-    const res = await request(app).get('/api/shorturl/1')
-
-    expect(res.statusCode).toBe(301)
-    expect(res.headers.location).toBe('https://www.google.com')
-  })
-
-  it('try getting non-existing url', async () => {
-    const res = await request(app).get('/api/shorturl/9999')
-
-    expect(res.statusCode).toBe(404)
-    expect(res.body).toEqual({ error: 'No url found for the given ID' })
-  })
-
-  it('try getting url with invalid id', async () => {
-    const res = await request(app).get('/api/shorturl/invalid-id')
-
-    expect(res.statusCode).toBe(400)
-    expect(res.body).toEqual({ error: 'Invalid ID' })
-  })
-})
